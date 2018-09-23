7944c4f88c97c88785eaf5a62f4868850c825807
Eden Ramoneda <edenlee143@gmail.com>
2018-09-23 21:39:23 +0800

    dropdown fixed

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,8 +1,7 @@
 
 
-
-        <script type="text/javascript" src="{{ url('js/bootstrap.min.js') }}"></script>
         <script type="text/javascript" src="{{ url('js/jquery.min.js') }}"></script>
+        <script type="text/javascript" src="{{ url('js/bootstrap.min.js') }}"></script>
         <script type="text/javascript" src="{{ url('js/style.js') }}"></script>
         <link rel="stylesheet" type="text/css" href="{{ url('css/bootstrap.min.css') }}">
         <link rel="stylesheet" type="text/css" href="{{ url('fonts/fontawesome-all.min.css') }}">
