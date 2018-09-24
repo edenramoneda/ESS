bbd50a01e883220049ed8bbd8af893b3ff07d09c
Eden Ramoneda <edenlee143@gmail.com>
2018-09-24 18:22:05 +0800

    adding content in payslip information

--- resources/views/Employee/modules/payslip.blade.php
+++ resources/views/Employee/modules/payslip.blade.php
@@ -3,12 +3,12 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-4 col-md-4 col-lg-4">
-                <div class="card employee-info mt-5">
+                <div class="card employee-info mt-5 payslip">
                     <div class="card-header text-white">
                         Payment
                     </div>
                      <div class="card-body" style="overflow-y:scroll">
-                        dsds
+                         <b>Basic:</b> <span>1001</span><br>
                     </div>
                 </div>
             </div>
@@ -28,7 +28,8 @@
                         Deduction
                     </div>
                      <div class="card-body" style="overflow-y:scroll">
-                        dsds
+                        <b>SSS:</b> <span>40</span><br>
+                        <b>PAG-IBIG:</b> <span>5</span>
                     </div>
                 </div>
             </div>            
@@ -37,10 +38,22 @@
            <div class="col-sm-12 col-md-12 col-lg-12">
                 <div class="card employee-info mt-5 payslip">
                     <div class="card-header text-white">
-                        Deduction
+                        Payslip Information 
+                            <button type="button" class="btn">Download PDF</button>&ensp;
+                            <button type="button" class="btn">Print</button>
+                        
                     </div>
                      <div class="card-body" style="overflow-y:scroll">
-                        dsds
+                        <b>Employee No:</b><span>120</span><br>
+                        <b>Name:</b> <span>Marc Carlos De Leon</span><br>
+                        <b>Department:</b> <span>I.T.</span><br>
+                        <b>Section:</b> <span>Maintenance</span><br>
+                        <b>Position:</b> <span>I.T. Manager</span><br>
+                        <b>Pay Date:</b> <span>2018-09-12</span><br>
+                        <b>Total Earnings:</b> <span>15000</span><br>
+                        <b>Total Benefits:</b> <span>50000</span><br>
+                        <b>Total Deductions:</b> <span>54700</span><br>
+                        <b>Net Amount:</b> <span>14000</span><br>
                     </div>
                 </div>
             </div>
